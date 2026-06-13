815562e45172411abc88e8aec51361dc9f59edbe
Ivan Andjelkovic <83224969+IvanAndjelkovic@users.noreply.github.com>
2026-06-13 12:13:26 +0000

    Combine the /contact route with /form-entry so that they are both under the route "/contact" but depending on which method (GET/POST) that triggered the route, handle it appropriately.

--- main.py
+++ main.py
@@ -15,23 +15,16 @@ def get_all_posts():
 @app.route("/about")
 def about():
     return render_template("about.html")
-
-
-@app.route("/contact")
+@app.route("/contact", methods=["GET","POST"])
 def contact():
+    if request.method == "POST":
+        data = request.form
+        print(data["name"])
+        print(data["email"])
+        print(data["phone"])
+        print(data["message"])
+        return "<h1>Successfully sent your message</h1>"
     return render_template("contact.html")
-
-@app.route("/form-entry", methods=["POST"])
-def receive_data():
-    name = request.form["name"]
-    email = request.form["email"]
-    phone = request.form["phone"]
-    message = request.form["message"]
-    print(name)
-    print(email)
-    print(phone)
-    print(message)  
-    return f"<h1>Successfully sent your message!</h1>"
 
 
 @app.route("/post/<int:index>")
